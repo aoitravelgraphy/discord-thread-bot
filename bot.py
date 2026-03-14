b4a9cd85745a01aa15c96b78a881be6d559795f6
小野葵 <onoaoi@AMac.local>
2026-03-15 06:02:01 +0900

    first commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"ログインしました: {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    try:
+        await message.create_thread(
+            name=message.content[:15],
+            auto_archive_duration=1440
+        )
+    except:
+        pass
+
+client.run(TOKEN)
